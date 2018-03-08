a071e5989d062dace91693660983aea006b2d6ca
Sangwon Lee <leee.sangwon@gmail.com>
2018-03-09 02:05:45 +0900

    Add: comparision plotting

--- plot_for_pspnet.py
+++ plot_for_pspnet.py
@@ -16,42 +16,66 @@ LR_MULT = 10000 # learning rate multiplyer
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("log_file_path", type=str, help="path to the log file to plot")
+    parser.add_argument("-c", "--comparision", type=str, default=None, help="path to the comparision log file to plot, default: not plot")
     parser.add_argument("-g", "--graph_title", type=str, default=None, help="title of the graph, default: the log file's name")
-    parser.add_argument("-i", "--interval", type=int, default=3000, help="plotting interval, unit: ms, default: 3000")
-    parser.add_argument("-d", "--dark_mode", action="store_true", help="dark mode turns on")
+    parser.add_argument("-i", "--interval", type=int, default=3000, help="plotting interval(ms), default: 3000")
+    parser.add_argument("-d", "--dark_mode", action="store_true", help="turns on the dark mode")
     args = parser.parse_args()
 
     log_file_path = args.log_file_path
+    assert os.path.isfile(log_file_path), "there is no log file at %s" % log_file_path
     graph_title = args.graph_title
     draw_rate = args.interval
+    comparision_log_file_path = args.comparision
     if args.dark_mode:
         style.use('dark_background')
     if graph_title is None:
         graph_title = os.path.basename(log_file_path)
-    plot(log_file_path, graph_title, draw_rate)
+    
+    plot(log_file_path, comparision_log_file_path, graph_title, draw_rate)
 
 
-def plot(log_file_path, graph_title, draw_rate, lr_mult=LR_MULT):
+def plot(log_file_path, comparision_log_file_path, graph_title, draw_rate, lr_mult=LR_MULT):
     fig = plt.figure()
-    ani = plot_iteratively(fig, log_file_path, draw_rate, lr_mult)
+    ax1, ax2 = draw_init(fig, lr_mult)
+    
+    loss_plot, = ax1.plot([], [], 'C1', label="total_loss")
+    lr_plot, = ax2.plot([], [], 'C2', label='learning_rate')
+    
+    if comparision_log_file_path is None:
+        plt.legend([loss_plot, lr_plot], ["loss", "lr"])
+    elif os.path.isfile(comparision_log_file_path):
+        comparision_loss_plot, = ax1.plot([], [], 'C3--', label="comparision_loss")
+        comparision_lr_plot, = ax2.plot([], [], 'C4--', label="comparision_lr")
+
+        plt.legend([loss_plot, lr_plot, comparision_loss_plot, comparision_lr_plot], ["loss", "lr", "comp_loss", "comp_lr"])
+
+        def plot_comparision():
+            with open(comparision_log_file_path, 'r') as log_file:
+                lines = log_file.read()
+            draw_once(lines, comparision_loss_plot, comparision_lr_plot, lr_mult)
+        
+        plot_comparision()
+    else:
+        print("no comparision file at %s" % comparision_log_file_path)
+        plt.legend([loss_plot, lr_plot], ["loss", "lr"])
+
+    def plot_iteratively():
+        def animate(frame):
+            with open(log_file_path, 'r') as log_file:
+                lines = log_file.read()
+            draw_once(lines, loss_plot, lr_plot, lr_mult)
+            if is_optimization_done(lines):
+                sys.exit()
+
+        return animation.FuncAnimation(fig, animate, interval=draw_rate)
+    
+    ani = plot_iteratively()
 
     plt.title(graph_title, fontsize=15)
     plt.grid(True)
     plt.subplots_adjust(left=0.12, right=0.85)
     plt.show()
-
-
-def plot_iteratively(fig, log_file_path, draw_rate, lr_mult):
-    ax1, ax2 = draw_init(fig, lr_mult)
-
-    def animate(i, *fargs):
-        with open(log_file_path, 'r') as log_file:
-            lines = log_file.read()
-        draw_once(lines, ax1, ax2, lr_mult)
-        if is_optimization_done(lines):
-            sys.exit()
-
-    return animation.FuncAnimation(fig, animate, interval=draw_rate)
 
 
 def draw_init(fig, lr_mult, xmax=STEP_MAX, loss_min=LOSS_MIN, loss_max=LOSS_MAX):
@@ -66,16 +90,12 @@ def draw_init(fig, lr_mult, xmax=STEP_MAX, loss_min=LOSS_MIN, loss_max=LOSS_MAX)
 
     return ax1, ax2
 
-def draw_once(lines, ax1, ax2, lr_mult, color_loss='r', color_lr='g'):
-    iter_list, loss_list, lr_list, aux1_list, aux2_list = parse_to_list(lines)
-     
-    loss_plot, = ax1.plot(iter_list, loss_list, color_loss, label="total_loss")
-    
+def draw_once(lines, loss_plot, lr_plot, lr_mult):
+    iter_list, loss_list, lr_list, _, _ = parse_to_list(lines)
+    loss_plot.set_data(iter_list, loss_list)
     lr_list = [x*lr_mult for x in lr_list]
-    lr_plot, = ax2.plot(iter_list, lr_list, color_lr, label="learning_rate")
+    lr_plot.set_data(iter_list, lr_list)
     
-    plt.legend([loss_plot, lr_plot], ["loss", "lr"])
-
 
 def parse_to_list(lines):
     re_iteration_line = re.compile(r"Iteration [\d]+, loss")
